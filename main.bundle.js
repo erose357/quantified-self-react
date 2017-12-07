68a55331c846228b4e63258f00097279718de74c
Becki Srofe <blf_23@yahoo.com>
2017-12-07 13:19:21 -0700

    Run build

--- main.bundle.js
+++ main.bundle.js
@@ -465,19 +465,19 @@
 
 	function appendFoods(data) {
 	  for (var i = 0; i < data.length; i++) {
-	    $('.headings').after('<tr class="food ' + data[i].id + '">\n          <td contenteditable="true">' + data[i].name + '</td>\n          <td contenteditable="true">' + data[i].calories + '</td>\n          <td class="delete-button"><button class="delete-food ' + data[i].id + '">-</button></td>\n        </tr>');
+	    $('.headings').after('<tr class="food ' + data[i].id + '">\n          <td tabindex="0" contenteditable="true">' + data[i].name + '</td>\n          <td tabindex="0" contenteditable="true">' + data[i].calories + '</td>\n          <td class="delete-button"><button class="delete-food ' + data[i].id + '">-</button></td>\n        </tr>');
 	  }
 	}
 
 	function appendFoodsDiary(data) {
 	  for (var i = 0; i < data.length; i++) {
-	    $('.headings-diary').after('<tr class="food ' + data[i].id + '">\n          <td class="food-checkbox"><input class="' + data[i].id + '" type="checkbox" name="food-checkbox" aria-labelledby="foods-table"></td>\n          <td>' + data[i].name + '</td>\n          <td>' + data[i].calories + '</td>\n        </tr>');
+	    $('.headings-diary').after('<tr class="food ' + data[i].id + '">\n          <td class="food-checkbox"><input class="' + data[i].id + '" type="checkbox" name="food-checkbox" aria-labelledby="foods-table"></td>\n          <td tabindex="0">' + data[i].name + '</td>\n          <td tabindex="0">' + data[i].calories + '</td>\n        </tr>');
 	  }
 	}
 
 	function appendFood(meal_object) {
 	  meal_object.foods.forEach(function (food) {
-	    $('.' + meal_object.name).after('<tr class="food' + food.id + ' meal' + meal_object.id + ' foodLoad">\n          <td>' + food.name + '</td>\n          <td class="countCalories">' + food.calories + '</td>\n          <td class="delete-button"><button class="delete-food ' + food.id + '">-</button></td>\n          </tr>');
+	    $('.' + meal_object.name).after('<tr class="food' + food.id + ' meal' + meal_object.id + ' foodLoad">\n          <td tabindex="0">' + food.name + '</td>\n          <td tabindex="0" class="countCalories">' + food.calories + '</td>\n          <td class="delete-button"><button class="delete-food ' + food.id + '">-</button></td>\n          </tr>');
 	  });
 	}
 
